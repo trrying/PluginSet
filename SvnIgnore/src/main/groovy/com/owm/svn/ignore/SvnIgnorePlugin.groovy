fdb40002769e738f75044c4c220386a21019bd8c
ouweiming <ouweiming@icefoxgame.com>
2019-08-28 15:23:45 +0800

    添加SvnIgnore插件1.0.1版本；

---
+++ SvnIgnore/src/main/groovy/com/owm/svn/ignore/SvnIgnorePlugin.groovy
@@ -0,0 +1,37 @@
+package com.owm.svn.ignore
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import groovy.xml.XmlUtil
+
+/**
+ * Created by "ouweiming" on 2019/8/28.
+ */
+class SvnIgnorePlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        def ignoredMap = [
+                ["name": "path", value: ".idea/"],
+                ["name": "path", value: ".gradle/"],
+                ["name": "path", value: "gradle/"],
+                ["name": "path", value: "build/"],
+                ["name": "path", value: "gradlew"],
+                ["name": "path", value: "gradlew.bat"],
+                ["name": "path", value: "local.properties"],
+                ["name": "mask", value: "*.iml"],
+        ]
+        project.rootDir.list().each { if (project.file("${project.rootDir}/$it/build").exists()) { ignoredMap.add(["name": "path", "value": "$it/build/"]) } }
+        File workspaceFile = project.file("${project.rootDir}/.idea/workspace.xml")
+        def workspace = new XmlParser().parseText(workspaceFile.getText())
+        def changeListManager = workspace.component.find { it.attribute("name") == "ChangeListManager" } ?: workspace.appendNode("component", ["name": "ChangeListManager"])
+        ignoredMap.each {
+            def ignoredNode = changeListManager.ignored.find { ignoredNode -> ignoredNode.attribute(it.name) == it.value }
+            if (ignoredNode == null) { changeListManager.appendNode("ignored", ["${it.name}" : "${it.value}"]) }
+        }
+        PrintWriter pw = new PrintWriter(workspaceFile)
+        pw.write(XmlUtil.serialize(workspace))
+        pw.close()
+    }
+
+}
